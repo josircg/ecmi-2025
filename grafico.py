8738e26c6c3b4dc99ce923eb6296db58cd84c8f1
Josir Cardoso Gomes <josircg@gmail.com>
2025-05-27 16:17:46 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -6,4 +6,18 @@ try:
 except:
   df = pd.read_csv('https://perfil-i.ibict.br/media/uploads/user_sum.csv')
 
-st.bar_chart(df, x='month', y='total')
+meses = df.groupby(df['month'].str[0:4]).count()
+lista = []
+for item in meses['month'].index:
+    lista.append(item)
+  
+option = st.selectbox(
+    "Escolha o ano", lista
+)
+
+if option:
+    data = df[df['month'].str.startswith('2024', na=False)]
+else:
+    data = df
+
+st.bar_chart(data, x='month', y='total')
