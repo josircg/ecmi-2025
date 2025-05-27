13ada11a1ccbcc1fd8ccd64e51478bc9218e2413
Josir Cardoso Gomes <josircg@gmail.com>
2025-05-27 15:30:29 -0300

    Create grafico.py

---
+++ grafico.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+try:
+  tot_registros = df.count()['total']
+except:
+  df = pd.read_csv('https://perfil-i.ibict.br/media/uploads/user_sum.csv')
+
+st.bar_chart(data=df x='month', y='total')
